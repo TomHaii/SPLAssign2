a8f6eace77362991b87c3f42159390faccc84917
TomL <tomlevy02@gmail.com>
2019-12-09 13:59:00 +0200

    input

--- src/test/java/bgu/spl/mics/FutureTest.java
+++ src/test/java/bgu/spl/mics/FutureTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FutureTest {
+    Future<String> future;
     @BeforeEach
     public void setUp(){
 
