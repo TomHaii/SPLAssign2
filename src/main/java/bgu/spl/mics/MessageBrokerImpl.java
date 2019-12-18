89708e7f75794ef535a33abfdf8ab8d9ed3be619
TomL <tomlevy02@gmail.com>
2019-12-18 16:28:37 +0200

    0_O

--- src/main/java/bgu/spl/mics/MessageBrokerImpl.java
+++ src/main/java/bgu/spl/mics/MessageBrokerImpl.java
@@ -1,7 +1,6 @@
 package bgu.spl.mics;
 
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.*;
 
 /**
  * The {@link MessageBrokerImpl class is the implementation of the MessageBroker interface.
@@ -13,10 +12,10 @@ public class MessageBrokerImpl implements MessageBroker {
 		private static MessageBroker instance = new MessageBrokerImpl();
 	}
 	private ConcurrentHashMap<Event<?>, Future<?>> futureMap;
-	private ConcurrentHashMap<Subscriber, LinkedBlockingDeque<Message>> subscriberList;
-	private ConcurrentHashMap<Subscriber, LinkedBlockingDeque<Class<? extends Message>>> topicsList;
-	private ConcurrentHashMap<Class<? extends Broadcast>, LinkedBlockingDeque<Subscriber>> broadcastMap;
-	private ConcurrentHashMap<Class<? extends Event<?>>, LinkedBlockingDeque<Subscriber>> eventMap;
+	private ConcurrentHashMap<Subscriber, LinkedBlockingQueue<Message>> subscriberList;
+	private ConcurrentHashMap<Subscriber, LinkedBlockingQueue<Class<? extends Message>>> topicsList;
+	private ConcurrentHashMap<Class<? extends Broadcast>, LinkedBlockingQueue<Subscriber>> broadcastMap;
+	private ConcurrentHashMap<Class<? extends Event<?>>, LinkedBlockingQueue<Subscriber>> eventMap;
 	/**
 	 * Retrieves the single instance of this class.
 	 */
@@ -33,50 +32,71 @@ public class MessageBrokerImpl implements MessageBroker {
 	@Override
 	public <T> void subscribeEvent(Class<? extends Event<T>> type, Subscriber m) {
 		if(!eventMap.containsKey(type)){
-			eventMap.put(type, new LinkedBlockingDeque<>());
+			eventMap.put(type, new LinkedBlockingQueue<>());
 		}
 		eventMap.get(type).add(m);
+		topicsList.get(m).add(type);
 	}
 
 	@Override
 	public void subscribeBroadcast(Class<? extends Broadcast> type, Subscriber m) {
 		if(!broadcastMap.containsKey(type)){
-			broadcastMap.put(type, new LinkedBlockingDeque<>());
+			broadcastMap.put(type, new LinkedBlockingQueue<>());
 		}
 		broadcastMap.get(type).add(m);
+		topicsList.get(m).add(type);
+
 	}
 
 	@Override
 	public <T> void complete(Event<T> e, T result) {
-		// TODO Auto-generated method stub
+		for(Event<?> event: futureMap.keySet()){
+			if(event.equals(e))
+				futureMap.get(e).resolve(result);
+		}
 	}
 
 	@Override
 	public void sendBroadcast(Broadcast b) {
-		// TODO Auto-generated method stub
+		LinkedBlockingQueue<Subscriber> tmpQ;
+		if(broadcastMap.containsKey(b.getClass())) {
+			tmpQ = broadcastMap.get(b.getClass());
+			while (!tmpQ.isEmpty()) {
+				Subscriber tmpSub = tmpQ.poll();
+				subscriberList.get(tmpSub).add(b);
+			}
+		}
 	}
 
-	
 	@Override
 	public <T> Future<T> sendEvent(Event<T> e) {
-		// TODO Auto-generated method stub
+		if(eventMap.containsKey(e.getClass()) && !eventMap.get(e.getClass()).isEmpty()){
+			synchronized (eventMap.get(e.getClass())){
+				Subscriber subToSendEvent = eventMap.get(e.getClass()).poll();
+				subscriberList.get(subToSendEvent).add(e);
+				Future<T> future = new Future<>();
+				futureMap.put(e,future);
+				eventMap.get(e.getClass()).add(subToSendEvent);
+				return future;
+			}
+		}
 		return null;
 	}
 
 	@Override
 	public void register(Subscriber m) {
-		subscriberList.put(m, new LinkedBlockingDeque<>());
-		topicsList.put(m, new LinkedBlockingDeque<>());
-		// TODO Auto-generated method stub
-
+		subscriberList.put(m, new LinkedBlockingQueue<>());
+		topicsList.put(m, new LinkedBlockingQueue<>());
 	}
 
 	@Override
 	public void unregister(Subscriber m) {
 		subscriberList.remove(m);
 		for(Class<? extends Message> type: topicsList.get(m)){
-			eventMap.get(type).remove(m);
-			broadcastMap.get(type).remove(m);
+			if(eventMap.containsKey(type))
+				eventMap.get(type).remove(m);
+			else
+				broadcastMap.get(type).remove(m);
 		}
 		topicsList.remove(m);
 	}
